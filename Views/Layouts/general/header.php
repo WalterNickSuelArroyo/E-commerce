c263ec0426be892074daba09ce1df5c23749ea49
Walter Nick Suel Arroyo <nicodehero21@outlook.com>
2023-06-04 18:41:17 -0500

    agregando cards para datos personales, dircciones de envio y tarjetas de pago en el apartado de mi perfil

--- Views/Layouts/general/header.php
+++ Views/Layouts/general/header.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href=../"Util/Css/css/all.min.css">
+  <link rel="stylesheet" href="../Util/Css/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../Util/Css/adminlte.min.css">
 </head>
